5a92fad3463a5ab4e8c92ee138b1f3edf15cdad5
Rafał Mikrut <mikrutrafal@protonmail.com>
2023-03-14 16:22:07 +0100

    First new

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use rand::{thread_rng, Rng};
-use rayon::prelude::*;
+use std::cmp::min;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::{env, fs, process};
+
+use rand::prelude::ThreadRng;
+use rand::{thread_rng, Rng};
+use rayon::prelude::*;
 
 const CHANGED_ELEMENTS: usize = 10;
 
@@ -39,12 +42,10 @@ fn main() {
                     process::exit(1);
                 }
             };
-            for entry in read_dir {
-                if let Ok(entry_data) = entry {
-                    if let Ok(metadata) = entry_data.metadata() {
-                        if metadata.is_file() {
-                            files_to_check.push(entry_data.path());
-                        }
+            for entry in read_dir.flatten() {
+                if let Ok(metadata) = entry.metadata() {
+                    if metadata.is_file() {
+                        files_to_check.push(entry.path());
                     }
                 }
             }
@@ -62,82 +63,117 @@ fn main() {
         process::exit(1);
     }
 
-    files_to_check = files_to_check.iter().filter(|e| e.to_string_lossy().to_string().contains('.')).cloned().collect();
+    let process_characters = false;
 
-    files_to_check.par_iter().for_each({
+    files_to_check.retain(|e| e.to_string_lossy().to_string().contains('.'));
+
+    files_to_check.into_par_iter().for_each({
         |file| {
-            let mut data_vector: Vec<u8>;
-            data_vector = match fs::read(&file) {
-                Ok(t) => t,
-                Err(_) => {
-                    println!("Failed to read data from file {:?}!", file);
-                    return;
-                }
+            let Some((full_name, file_name, extension)) = read_file_name_extension(file) else {
+                return;
             };
 
-            let file_as_str = file.to_string_lossy().to_string();
-            let dot_index = match file_as_str.rfind('.') {
-                Some(t) => t,
-                None => {
-                    println!("File {} doesn't contains a required dot", file_as_str);
-                    return;
-                }
-            };
-
-            let mut random_values: [u8; CHANGED_ELEMENTS] = [0; CHANGED_ELEMENTS];
-            let mut old_values: [u8; CHANGED_ELEMENTS] = [0; CHANGED_ELEMENTS];
-            let mut random_indexes: [usize; CHANGED_ELEMENTS] = [0; CHANGED_ELEMENTS];
-
-            for i in 0..number_of_copies {
-                for j in 0..CHANGED_ELEMENTS {
-                    if data_vector.len() == 0 {
-                        continue;
-                    }
-                    let random_index = thread_rng().gen_range(0..data_vector.len());
-                    let random_value = thread_rng().gen_range(0..=255u8);
-                    random_indexes[j] = random_index;
-                    random_values[j] = random_value;
-
-                    old_values[j] = data_vector[random_index];
-
-                    data_vector[random_index] = random_value;
-                }
-
-                let new_file_name: String = format!("{}{}{}", &file_as_str[..dot_index], i, &file_as_str[dot_index..]);
-
-                let mut file_handler = match OpenOptions::new().create(true).write(true).open(&new_file_name) {
-                    Ok(t) => t,
-                    Err(_) => {
-                        for j in 0..CHANGED_ELEMENTS {
-                            data_vector[random_indexes[j]] = old_values[j];
-                        }
-                        println!("Failed to create file {}", new_file_name);
-                        continue;
-                    }
-                };
-                if data_vector.len() != 0 {
-                    let size;
-                    // Small change to randomly split
-                    if thread_rng().gen_range(0..10) == 0 {
-                        size = thread_rng().gen_range(0..data_vector.len());
-                    } else {
-                        size = data_vector.len();
-                    }
-                    // Normal full saving
-
-                    if file_handler.write(&(data_vector[..size])).is_err() {
-                        for j in 0..CHANGED_ELEMENTS {
-                            data_vector[random_indexes[j]] = old_values[j];
-                        }
-                        println!("Failed to save data to file {}", new_file_name);
-                        continue;
-                    }
-
-                    for j in 0..CHANGED_ELEMENTS {
-                        data_vector[random_indexes[j]] = old_values[j];
-                    }
-                }
+            if process_characters {
+            } else {
+                process_bytes(full_name, file_name, extension, number_of_copies);
             }
         }
     });
+}
+
+fn process_bytes(original_name: String, file_name: String, extension: String, repeats: u64) {
+    let data_vector: Vec<u8> = match fs::read(&original_name) {
+        Ok(t) => t,
+        Err(_) => {
+            println!("Failed to read data from file {:?}!", original_name);
+            return;
+        }
+    };
+    let mut thread_rng = thread_rng();
+
+    for i in 0..repeats {
+        let new_file_name = format!("{file_name}{i}.{extension}");
+        let mut file_handler = match OpenOptions::new().create(true).write(true).open(&new_file_name) {
+            Ok(t) => t,
+            Err(_) => {
+                println!("Failed to create file {}", new_file_name);
+                continue;
+            }
+        };
+
+        let mut data = data_vector.clone();
+        if data.is_empty() {
+            continue;
+        }
+
+        split_content(&mut thread_rng, &mut data, 0.5);
+        if data.is_empty() {
+            continue;
+        }
+
+        let changed_items = min(data.len() / 5, 5);
+        remove_random_items(&mut thread_rng, &mut data, changed_items);
+        if data.is_empty() {
+            continue;
+        }
+
+        let changed_items = min(data.len() / 5, 5);
+        remove_random_items(&mut thread_rng, &mut data, changed_items);
+        if data.is_empty() {
+            continue;
+        }
+
+        if file_handler.write(&data).is_err() {
+            println!("Failed to save data to file {}", new_file_name);
+            continue;
+        }
+    }
+}
+// fn process_general<T>(data: Vec<T>, file_name: String, extension: String, repeats: u64) {
+//
+// }
+
+fn split_content<T>(rng: &mut ThreadRng, data: &mut Vec<T>, probability: f32) {
+    let idx = get_random_idx(rng, data.as_slice());
+    let size = if rng.gen_range(0.0..100.0) < probability { idx } else { data.len() };
+    data.truncate(size)
+}
+
+fn remove_random_items<T>(rng: &mut ThreadRng, data: &mut Vec<T>, item_number: usize) {
+    for _ in 0..item_number {
+        let idx = get_random_idx(rng, data.as_slice());
+        data.remove(idx);
+    }
+}
+
+fn modify_random_bytes<T>(rng: &mut ThreadRng, data: &mut Vec<T>, item_number: usize)
+where
+    rand::distributions::Standard: rand::distributions::Distribution<T>,
+{
+    for _ in 0..item_number {
+        let idx = get_random_idx(rng, data.as_slice());
+        data[idx] = rng.gen::<T>();
+    }
+}
+
+fn get_random_idx<T>(rng: &mut ThreadRng, data: &[T]) -> usize {
+    rng.gen_range(0..data.len())
+}
+
+fn read_file_name_extension(file: PathBuf) -> Option<(String, String, String)> {
+    let full_name = match file.to_str() {
+        Some(k) => k.to_string(),
+        None => {
+            println!("File not have valid UTF-8 name {:?}!", file);
+            return None;
+        }
+    };
+    let dot_index = match full_name.rfind('.') {
+        Some(t) => t,
+        None => {
+            println!("File {} doesn't contains a required dot", full_name);
+            return None;
+        }
+    };
+    Some((full_name.clone(), full_name[..dot_index].to_string(), full_name[(dot_index + 1)..].to_string()))
 }
